8346966e95471e0a9aadaeb2058b4fae282e5c25
Felipe Baisi <febaisi@gmail.com>
2016-04-28 09:22:08 -0300

    Adding parseJsonMovieInfo
    Signed-off-by: Felipe Baisi <febaisi@gmail.com>

--- app/src/main/java/com/febaisi/moviesearch/controller/MovieInfoController.java
+++ app/src/main/java/com/febaisi/moviesearch/controller/MovieInfoController.java
@@ -1,14 +1,10 @@
 package com.febaisi.moviesearch.controller;
 
 import android.content.Context;
-import android.util.Log;
 
-import com.android.volley.Response;
-import com.febaisi.moviesearch.R;
 import com.febaisi.moviesearch.model.Movie;
 import com.febaisi.moviesearch.model.MovieInfo;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -39,9 +35,25 @@ public class MovieInfoController extends MovieController {
 
     private MovieInfo parseJsonMovieInfo(JSONObject jsonObject) throws JSONException {
         MovieInfo movieInfo = new MovieInfo();
-        movieInfo.setTitle(jsonObject.getString(Movie.TITLE));
-
-
+        movieInfo.setTitle(jsonObject.getString(MovieInfo.TITLE));
+        movieInfo.setYear(jsonObject.getString(MovieInfo.YEAR));
+        movieInfo.setRated(jsonObject.getString(MovieInfo.RATED));
+        movieInfo.setRuntime(jsonObject.getString(MovieInfo.RUNTIME));
+        movieInfo.setGenre(jsonObject.getString(MovieInfo.GENRE));
+        movieInfo.setDirector(jsonObject.getString(MovieInfo.DIRECTOR));
+        movieInfo.setWriter(jsonObject.getString(MovieInfo.WRITER));
+        movieInfo.setActors(jsonObject.getString(MovieInfo.ACTORS));
+        movieInfo.setPlot(jsonObject.getString(MovieInfo.PLOT));
+        movieInfo.setLanguage(jsonObject.getString(MovieInfo.LANGUAGE));
+        movieInfo.setCountry(jsonObject.getString(MovieInfo.COUNTRY));
+        movieInfo.setAwards(jsonObject.getString(MovieInfo.AWARDS));
+        movieInfo.setPoster(jsonObject.getString(MovieInfo.POSTER));
+        movieInfo.setMetascore(jsonObject.getString(MovieInfo.METASCORE));
+        movieInfo.setImdbRating(jsonObject.getString(MovieInfo.IMDB_RATING));
+        movieInfo.setImdbVotes(jsonObject.getString(MovieInfo.IMDB_VOTES));
+        movieInfo.setImdbID(jsonObject.getString(MovieInfo.IMDB_ID));
+        movieInfo.setType(jsonObject.getString(MovieInfo.TYPE));
+        movieInfo.setResponse(jsonObject.getString(MovieInfo.RESPONSE));
 
         return movieInfo;
     }
